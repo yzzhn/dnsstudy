89bcc3ef4e56a4ad660ea9b6f81e463eafc59146
yz6me <yz6me@virginia.edu>
2024-02-28 19:32:49 +0000

    config change

--- code/config.py
+++ code/config.py
@@ -22,7 +22,7 @@ NSIP_PARSED = "/data/nsIP/parsed"
 NSIP_DB = "/data/nsIP/NameServerIP.db"
 
 # default concurrency for query 
-MAXCONCURRENCY = 40
+MAXCONCURRENCY = 30
 
 # default DNS servers
 RESOLVER_LIST = ['8.8.8.8', '1.1.1.1']
